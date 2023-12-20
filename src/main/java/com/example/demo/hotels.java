82822965cb35fa6b6c38f6195b85ad8cd054927b
yashrandhe16 <yash@techwingsolutions.com>
2023-12-20 14:00:05 +0530

    adding hotel section

---
+++ src/main/java/com/example/demo/hotels.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotels {
+    @GetMapping("/hotels")
+    public String getData() {return  "Please book your hotel at 20% discount at Goibibo" ; }
+}
